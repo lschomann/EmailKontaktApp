e98d5d961ee5f4cde52f05ae1d4ecc13c8b1ca38
unknown <fa11engel@Wk15_04.szut.uni-bremen.de>
2012-10-02 15:06:16 +0200

    initail implementation of Sqlite dao

--- src/DataLayer/DataAccessObjects/Sqlite/EmailKontaktDaoSqlite.java
+++ src/DataLayer/DataAccessObjects/Sqlite/EmailKontaktDaoSqlite.java
@@ -15,14 +15,39 @@ import java.sql.*;
 public class EmailKontaktDaoSqlite implements IEmailKontaktDAO{
 
 	public void Init(){
+		try{
+		    Connection conn = getConnection();
+		    Statement stat = conn.createStatement();
+		    stat.executeUpdate("CREATE TABLE kontakte(id integer primary key, vorname, nachname, email);");
+		    PreparedStatement stmt = conn.prepareStatement("insert into kontakte values (null, ?, ?, ?);");
+		    
+            stmt.setString(2,"Richard");
+            stmt.setString(3,"Lionheart");
+            stmt.setString(4,"rich@king.co.uk");
+            stmt.addBatch();
+            
+            stmt.execute();
+		    
+		    conn.commit();
+		    conn.close();
+		}
+	}
+	
+	public Connection getConnection(){
 		String connstr = "jdbc:sqlite:kontakte_db";
 		String user = "master";
 		String pw = "1234";
 		
 		try{
+			Class.forName("org.sqlite.JDBC");
+		    return DriverManager.getConnection("jdbc:sqlite:test.db");
+		}
+		catch(SQLException ex){
 			
 		}
+		
 	}
+	
 	
     @Override
     public IEmailKontakt create() {
@@ -31,6 +56,13 @@ public class EmailKontaktDaoSqlite implements IEmailKontaktDAO{
 
     @Override
     public IEmailKontakt[] select() {
+    	ResultSet rs = stat.executeQuery("select * from people;");
+    	while (rs.next())
+	    {
+	      System.out.println("name = " + rs.getString("name"));
+	      System.out.println("job = " + rs.getString("occupation"));
+	    }
+	    rs.close();*/
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
