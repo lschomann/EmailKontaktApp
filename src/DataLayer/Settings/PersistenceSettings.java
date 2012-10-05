865cfd349748db8284b34435b412bb4a5067c9d1
unknown <fa11ritter@W101_69.szut.uni-bremen.de>
2012-10-05 09:39:23 +0200

    Code hinzugefügt

--- src/DataLayer/Settings/PersistenceSettings.java
+++ src/DataLayer/Settings/PersistenceSettings.java
@@ -4,6 +4,12 @@
  */
 package DataLayer.Settings;
 
+import java.io.IOException;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+
 /**
  *
  * @author lschomann
@@ -12,7 +18,17 @@ public class PersistenceSettings {
     
     private String type;
 
-    public String getPersistenceType(){
+    public String getPersistenceType() throws ParserConfigurationException, SAXException, 
+    IOException, XPathExpressionException{
+    	
+    	DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+    	domFactory.setNamespaceAware(true);
+    	DocumentBuilder docBuilder = domFactory.newDocumentBuilder();
+    	Document doc = docBuilder.parse("settings.xml");
+    	
+    	XPathFactory xpFactory = XPathFactory.newInstance();
+    	XPath xpath = xpFactory.newXPath();
+    	XPathExpression xpExpr = xpath.compile("//settings[option=)
         return type;
     }
     public void setPersistenceType(String type){
